c56782325cb09f896544636a8edc96dce007fda7
walter-temporal <walter.temporal@gmail.com>
2020-07-15 18:09:17 -0300

    added update travis DEPLOY file

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
-          bye there!
+          I was changed in feature branch!
         </p>
       </div>
     );
